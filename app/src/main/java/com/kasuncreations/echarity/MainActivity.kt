29c69b2552ce7ef4963b780a0c27c0ca4c6d7098
Kasun Thilina Thennakoon <kasun.thilina.emedia@gmail.com>
2020-02-10 09:47:54 +0530

    Implemented Login Screen

--- app/src/main/java/com/kasuncreations/echarity/MainActivity.kt
+++ app/src/main/java/com/kasuncreations/echarity/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.kasuncreations.echarity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor =
+                ContextCompat.getColor(this, R.color.colorPurple)
+        }
     }
 }
