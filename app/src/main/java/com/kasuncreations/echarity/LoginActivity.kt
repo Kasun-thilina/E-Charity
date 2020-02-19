7d6e29f0f7f93837dc22689634b0ff1851374e96
Kasun Thilina Thennakoon <kasun.thilina.emedia@gmail.com>
2020-02-19 14:45:35 +0530

    Added butterknife

--- app/src/main/java/com/kasuncreations/echarity/LoginActivity.kt
+++ app/src/main/java/com/kasuncreations/echarity/LoginActivity.kt
@@ -1,28 +1,42 @@
 package com.kasuncreations.echarity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.layout_bottom_sheet_sign_up.*
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ButterKnife.bind(this)
+
         init()
     }
 
     private fun init() {
-        bottom_sheet.visibility = View.GONE
+        //bottom_sheet.visibility = View.GONE
+/*
         val bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
-        btn_login.setOnClickListener {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        tv_signup.setOnClickListener {
             showToastLong("Clicked!")
             if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
                 bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
             } else {
                 bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
+            }
+
+        }*/
+    }
+
+    @OnClick(R.id.tv_q_signup)
+    internal fun click(view: View) {
+        when (view.id) {
+            R.id.tv_signup -> {
+                startActivity(Intent(this, SignUpActivity::class.java))
             }
 
         }
